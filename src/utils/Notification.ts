7b07f0c9a584a1d7a768a27177209638da73b5e8
rai <tjd123123@naver.com>
2023-05-03 20:54:47 +0900

    test : notification permission

--- src/utils/Notification.ts
+++ src/utils/Notification.ts
@@ -12,6 +12,7 @@ export const requestPermission = async () => {
         if (permission === "granted") {
             const token = await getToken(messaging, { vapidKey: "BBsUF1frFhq8n107d6OnIKNbKbRWtwhzSyf96rqxbnyu_f0oixl7SW7rHdxnVt938zyKyyAR4KtSzyzxWWgQ3zY" });
 
+            alert("token : " + token);
             onMessage(messaging, (payload) => {
                 console.log("Message received. ", payload);
 

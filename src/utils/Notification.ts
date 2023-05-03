2e344f71068dd1f6b1598cbcfbef9fef5cdd14b2
rai <tjd123123@naver.com>
2023-05-03 21:00:24 +0900

    test : notification permission

--- src/utils/Notification.ts
+++ src/utils/Notification.ts
@@ -4,30 +4,30 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 export const fcmToken = { token: "" };
 
 export const requestPermission = async () => {
-    if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
-        const messaging = getMessaging(app);
+    // if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
+    const messaging = getMessaging(app);
 
-        const permission = await Notification.requestPermission();
-        alert("requestPermission : " + permission);
+    const permission = await Notification.requestPermission();
+    alert("requestPermission : " + permission);
 
-        if (permission === "granted") {
-            const token = await getToken(messaging, { vapidKey: "BBsUF1frFhq8n107d6OnIKNbKbRWtwhzSyf96rqxbnyu_f0oixl7SW7rHdxnVt938zyKyyAR4KtSzyzxWWgQ3zY" });
+    if (permission === "granted") {
+        const token = await getToken(messaging, { vapidKey: "BBsUF1frFhq8n107d6OnIKNbKbRWtwhzSyf96rqxbnyu_f0oixl7SW7rHdxnVt938zyKyyAR4KtSzyzxWWgQ3zY" });
 
-            alert("token : " + token);
-            onMessage(messaging, (payload) => {
-                console.log("Message received. ", payload);
+        alert("token : " + token);
+        onMessage(messaging, (payload) => {
+            console.log("Message received. ", payload);
 
-                const notification = new Notification(payload.notification?.title ?? "", { body: payload.notification?.body });
-                notification.onclick = () => (location.href = "/");
-            });
+            const notification = new Notification(payload.notification?.title ?? "", { body: payload.notification?.body });
+            notification.onclick = () => (location.href = "/");
+        });
 
-            fcmToken.token = token;
-        } else if (permission === "denied") {
-            console.log("denied");
-            alert("denied");
-            fcmToken.token = "";
-        }
-    } else {
+        fcmToken.token = token;
+    } else if (permission === "denied") {
+        console.log("denied");
+        alert("denied");
         fcmToken.token = "";
     }
+    // } else {
+    // fcmToken.token = "";
+    // }
 };

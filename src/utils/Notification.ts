f6291cdb80db883ed813269cebc9cd017b0d0d15
rai <tjd123123@naver.com>
2023-05-04 14:43:25 +0900

    refactor : notification permission promise => callback 변경

--- src/utils/Notification.ts
+++ src/utils/Notification.ts
@@ -7,25 +7,25 @@ export const requestPermission = async () => {
     if (typeof window !== "undefined" && typeof window.navigator !== "undefined") {
         const messaging = getMessaging(app);
 
-        const permission = await Notification.requestPermission();
+        Notification.requestPermission().then(async (permission) => {
+            alert(permission);
 
-        if (permission === "granted") {
-            const token = await getToken(messaging, { vapidKey: "BBsUF1frFhq8n107d6OnIKNbKbRWtwhzSyf96rqxbnyu_f0oixl7SW7rHdxnVt938zyKyyAR4KtSzyzxWWgQ3zY" });
+            if (permission === "granted") {
+                const token = await getToken(messaging, { vapidKey: "BBsUF1frFhq8n107d6OnIKNbKbRWtwhzSyf96rqxbnyu_f0oixl7SW7rHdxnVt938zyKyyAR4KtSzyzxWWgQ3zY" });
 
-            alert("token : " + token);
-            onMessage(messaging, (payload) => {
-                console.log("Message received. ", payload);
+                onMessage(messaging, (payload) => {
+                    console.log("Message received. ", payload);
 
-                const notification = new Notification(payload.notification?.title ?? "", { body: payload.notification?.body });
-                notification.onclick = () => (location.href = "/");
-            });
+                    const notification = new Notification(payload.notification?.title ?? "", { body: payload.notification?.body });
+                    notification.onclick = () => (location.href = "/");
+                });
 
-            fcmToken.token = token;
-        } else if (permission === "denied") {
-            console.log("denied");
-            alert("denied");
-            fcmToken.token = "";
-        }
+                fcmToken.token = token;
+            } else if (permission === "denied") {
+                console.log("denied");
+                fcmToken.token = "";
+            }
+        });
     } else {
         fcmToken.token = "";
     }
